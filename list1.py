78cb8accadadf6604e1dbfa3fd8058b08fe41160
unknown <shettyabhijit930@gmail.com>
2019-07-23 12:11:05 +0530

    updated

---
+++ list1.py
@@ -0,0 +1,6 @@
+lis_1=[1,2,3,4]
+lis_2=[4,5,6,7]
+lis_3=[]
+for i in range(len(lis_1)):
+    lis_3.append(lis_1[i]+lis_2[i])
+print(lis_3)
---
+++ ran2.py
@@ -0,0 +1,7 @@
+import random as rn
+lst = []
+for i in range(1,101):
+    ele=(rn.randint(1,1000)
+    if ele%3==0 and ele%6==0 and not ele%9=0:
+        lst.append(ele)
+print(lst)        
